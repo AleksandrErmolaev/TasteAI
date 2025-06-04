83de0f6df9f68eb22c271b8b2bfbc046a264c221
Almaz <almaz.nasrutdinov@gmail.com>
2025-06-04 11:15:45 +0300

    изменение логики поиска ингредиентов ()

--- Model/synonym_finder.py
+++ Model/synonym_finder.py
@@ -1,29 +1,27 @@
 import re
 from fuzzywuzzy import fuzz
-
-
-def find_user_ingredients(text, all_ingredients, synonyms, threshold=70):
-
+def find_user_ingredients(text, all_ingredients, synonyms, threshold=60):
     text = str(text).lower().strip()
     found = set()
 
+    user_words = re.findall(r'\w+', text)
+
     for ing in all_ingredients:
-        if re.search(r'\b' + re.escape(ing) + r'\b', text):
+        if any(ing in word or word in ing for word in user_words):
             found.add(ing)
 
     for main_ing, syns in synonyms.items():
         for syn in syns:
-            if re.search(r'\b' + re.escape(syn) + r'\b', text) and main_ing not in found:
+            if any(syn in word or word in syn for word in user_words) and main_ing not in found:
                 found.add(main_ing)
 
     if len(found) < 3:
-        words = re.findall(r'\w+', text)
-        for word in words:
+        for word in user_words:
             matches = []
             for ing in all_ingredients:
-                if fuzz.token_set_ratio(word, ing) >= threshold:
-                    matches.append((ing, fuzz.token_set_ratio(word, ing)))
-
+                score = fuzz.partial_ratio(word, ing)
+                if score >= threshold:
+                    matches.append((ing, score))
             if matches:
                 best_match = max(matches, key=lambda x: x[1])[0]
                 found.add(best_match)
